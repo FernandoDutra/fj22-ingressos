8cf81eb249023641159d3b318d15c769ab34e963
FernandoAlvesDutra <fernandoalvesdutra@hotmail.com>
2019-03-25 16:12:58 -0300

    Mensagem explicando o trabalho que foi feito

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,37 @@
+package br.com.caelum.ingresso.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+
+@Controller
+public class SessaoController {
+
+	@Autowired
+	private FilmeDao filmeDao;
+	@Autowired
+	private SalaDao salaDao;
+
+	@GetMapping("admin/sessao")
+	public ModelAndView form(@RequestParam("salad") Integer salaId) {
+		ModelAndView mav = new ModelAndView("sessao/sessao");
+		
+		List<Filme> filmes = filmeDao.findAll();
+		Sala sala = salaDao.findOne(salaId);
+
+		mav.addObject("sala", sala);
+		mav.addObject("filmes", filmes);
+		
+		return mav;
+	}
+
+}
---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,48 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+public class Sessao {
+		
+	private Long id;
+	private Filme filme;
+	private Sala sala;
+	private LocalTime horario;	
+	
+	public Sessao(Filme filme, Sala sala, LocalTime horario) {
+		super();
+		this.filme = filme;
+		this.sala = sala;
+		this.horario = horario;
+	}
+	public LocalTime getHorarioTermino() {
+		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	
+	
+
+}
