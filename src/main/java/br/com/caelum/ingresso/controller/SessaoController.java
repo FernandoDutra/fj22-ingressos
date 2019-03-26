ba5f70b8391b4df2720a655b81831ec4796746c8
FernandoAlvesDutra <fernandoalvesdutra@hotmail.com>
2019-03-26 11:59:27 -0300

    Inserção da Sessao no banco de dados

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -2,16 +2,24 @@ package br.com.caelum.ingresso.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.form.SessaoForm;
 
 @Controller
 public class SessaoController {
@@ -20,17 +28,38 @@ public class SessaoController {
 	private FilmeDao filmeDao;
 	@Autowired
 	private SalaDao salaDao;
+	@Autowired
+	private SessaoDao sessaoDao;
 
 	@GetMapping("admin/sessao")
-	public ModelAndView form(@RequestParam("salad") Integer salaId) {
+	public ModelAndView form(@RequestParam("salad") Integer salaId, SessaoForm sessaoForm) {
+		sessaoForm.setSalaID(salaId);
+
 		ModelAndView mav = new ModelAndView("sessao/sessao");
-		
+
 		List<Filme> filmes = filmeDao.findAll();
 		Sala sala = salaDao.findOne(salaId);
 
 		mav.addObject("sala", sala);
 		mav.addObject("filmes", filmes);
+		mav.addObject("form", sessaoForm);
+
+		return mav;
+	}
+	
+	@PostMapping("admin/sessao")
+	@Transactional
+	public ModelAndView salva(@Valid SessaoForm sessaoForm, BindingResult result) {
+		if (result.hasErrors()) {
+			return form(sessaoForm.getSalaID(), sessaoForm);
+		}
 		
+		Sessao sessao = sessaoForm.toSessao(salaDao, filmeDao);
+		sessaoDao.save(sessao);
+		
+		// vai para a lista de sessões
+		ModelAndView mav = new ModelAndView("redirect:admin/sala/" + sessao.getSala().getId() + "/sessoes/");
+
 		return mav;
 	}
 
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,47 +2,76 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity // Anotação de Persistência da JPA (Hibernate)
 public class Sessao {
-		
+
+	@Id
+	@GeneratedValue
 	private Long id;
+	@ManyToOne
 	private Filme filme;
+	@ManyToOne
 	private Sala sala;
-	private LocalTime horario;	
-	
+	private LocalTime horario;
+
+	// Design Pattern JavaBean 
+	//(Método Sem argumentos, Getters e Setters para atributos privados)
+
+	// Anotação para deixar o construtor obsoleto (informar ao programador para não utilizar o método)
+	/**
+	 * @deprecated Só existe devido o Hibernate
+	 */
+	public Sessao() {
+
+	}
+
 	public Sessao(Filme filme, Sala sala, LocalTime horario) {
 		super();
 		this.filme = filme;
 		this.sala = sala;
 		this.horario = horario;
 	}
+
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Filme getFilme() {
 		return filme;
 	}
+
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
+
 	public Sala getSala() {
 		return sala;
 	}
+
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
+
 	public LocalTime getHorario() {
 		return horario;
 	}
+
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
-	
-	
 
 }
